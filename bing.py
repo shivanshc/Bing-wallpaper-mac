0a5c6650a6bd544b8c672c6e952f478bb0c511a7
shivanshc <shivansh_sc@yahoo.co.in>
2016-07-17 03:17:53 -0500

    Got the URL

--- bing.py
+++ bing.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
-r = requests.get("http://www.bing.com")
-soup = BeautifulSoup(r.text, "html.parser")
-text= (soup.prettify()).encode('utf-8')
+from PIL import Image
+from io import StringIO
+
+resp = requests.get("http://www.bing.com")
+soup = BeautifulSoup(resp.text, "html.parser")
 instance = soup.find_all("script", {"type": "text/javascript"})
 for i in instance:
-	if "g_img" in i:
-		print i
+    jsContent = i.text
+    if "g_img={url:" in jsContent:
+        beginIndex = jsContent.find('g_img={url: "') + 13
+        endIndex = jsContent.find('jpg', beginIndex) + 3
+        imgURL = jsContent[beginIndex:endIndex]
+        imgURL = "http://www.bing.com/" + imgURL
+
+resp = requests.get(imgURL)
+print type(resp.content)
+i = Image.open((resp.content))
+
+
