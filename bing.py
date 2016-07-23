078b4cbe444a97fd7530d60fc127cec3192c13ce
shivanshc <shivansh_sc@yahoo.co.in>
2016-07-23 02:38:55 -0500

    final done

--- bing.py
+++ bing.py
@@ -1,16 +1,57 @@
 from bs4 import BeautifulSoup
 import requests
 import wget
+import sqlite3
+import subprocess
+import os
 
-resp = requests.get("http://www.bing.com")
-soup = BeautifulSoup(resp.text, "html.parser")
-instance = soup.find_all("script", {"type": "text/javascript"})
-for i in instance:
-    jsContent = i.text
-    if "g_img={url:" in jsContent:
-        beginIndex = jsContent.find('g_img={url: "') + 13
-        endIndex = jsContent.find('jpg', beginIndex) + 3
-        imgURL = jsContent[beginIndex:endIndex]
-        imgURL = "http://www.bing.com/" + imgURL
+DB_PATH = 'Library/Application Support/Dock/desktoppicture.db'
+IMAGE_FOLDER = 'Library/Application Support/Dock/Bing'
+USER_DIR = os.getenv("HOME")
 
-wget.download(imgURL)
+
+def getImageURL():
+    resp = requests.get("http://www.bing.com")
+    soup = BeautifulSoup(resp.text, "html.parser")
+    instance = soup.find_all("script", {"type": "text/javascript"})
+    for i in instance:
+        jsContent = i.text
+        if "g_img={url:" in jsContent:
+            beginIndex = jsContent.find('g_img={url: "') + 13
+            endIndex = jsContent.find('jpg', beginIndex) + 3
+            imgURL = jsContent[beginIndex:endIndex]
+            imgURL = "http://www.bing.com/" + imgURL
+    return imgURL
+
+
+def downloadImage(downloadDir):
+    imgURL = getImageURL()
+    fileName = wget.download(imgURL, out=downloadDir, bar=False)
+    return fileName
+
+
+def updateBackground(imgPath):
+    dbPath = os.path.join(USER_DIR, DB_PATH)
+    conn = sqlite3.connect(dbPath)
+    c = conn.cursor()
+    c.execute('UPDATE data SET value=?', (imgPath,))
+    conn.commit()
+    conn.close()
+
+
+def getDownloadPath():
+    folderPath = os.path.join(USER_DIR, IMAGE_FOLDER)
+    if not os.path.isdir(folderPath):
+        os.mkdir(folderPath)
+    return folderPath
+
+
+def setBackground():
+    downloadDir = getDownloadPath()
+    imagePath = downloadImage(downloadDir)
+    updateBackground(imagePath)
+    subprocess.run(['killall', 'Dock'])
+
+
+if __name__ == '__main__':
+    setBackground()
